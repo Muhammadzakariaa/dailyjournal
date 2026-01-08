856c0f55999bb2b5baa0c1e219ccc5795b40dc0f
Muhammadzakariaa <muhammadzakaria3008@gmail.com>
2026-01-08 17:54:37 +0700

    update title

--- index.php
+++ index.php
@@ -8,7 +8,8 @@ include "koneksi.php";
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Pemandangan Alam Indonesia Bootstrap demo</title>
+    <title> My Daily Journal | Home</title>
+    <link rel="icon" href="image/logo.png"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     
      <!-- Script Typing -->
