7bd2975b36d6d92f752fae7a155309af73d701df
Muhammadzakariaa <muhammadzakaria3008@gmail.com>
2026-01-15 21:22:18 +0700

    update database

--- webdailyjournal.sql
+++ webdailyjournal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 07 Jan 2026 pada 11.06
+-- Waktu pembuatan: 15 Jan 2026 pada 15.16
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -50,6 +50,32 @@ INSERT INTO `article` (`id`, `judul`, `isi`, `gambar`, `tanggal`, `username`) VA
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `gallery`
+--
+
+CREATE TABLE `gallery` (
+  `id` int(11) NOT NULL,
+  `judul` text NOT NULL,
+  `isi` text NOT NULL,
+  `gambar` text NOT NULL,
+  `tanggal` datetime NOT NULL,
+  `username` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `gallery`
+--
+
+INSERT INTO `gallery` (`id`, `judul`, `isi`, `gambar`, `tanggal`, `username`) VALUES
+(1, 'Pantai', 'Pantai adalah tempat bertemunya daratan dan laut, dengan hamparan pasir, debur ombak, dan angin laut yang menenangkan', '20260115160142.jpg', '2026-01-15 16:01:42', 'april'),
+(2, 'Sungai', 'Sungai adalah aliran air yang mengalir dari hulu ke hilir, menghidupi alam sekitar dan menjadi sumber kehidupan', 'sungai.jpg', '2025-11-19 08:44:18', 'admin'),
+(3, 'Hutan', 'Hutan merupakan kawasan hijau yang dipenuhi pepohonan lebat, menjadi rumah bagi berbagai makhluk hidup dan penjaga keseimbangan alam', 'hutan.jpg', '2025-11-19 08:44:18', 'admin'),
+(4, 'Matahari', 'Matahari adalah sumber cahaya dan panas bagi bumi, memberi energi dan kehidupan bagi seluruh makhluk', 'matahari.jpg', '2025-11-19 08:47:13', 'admin'),
+(5, 'Kawah', 'Kawah adalah cekungan di permukaan bumi yang terbentuk akibat aktivitas vulkanik, sering mengeluarkan gas, asap, atau air panas, dan menjadi tanda kekuatan alam dari dalam bumi', 'kawah.jpg', '2025-11-19 08:47:13', 'admin');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `user`
 --
 
@@ -65,7 +91,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `foto`) VALUES
-(1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', '');
+(1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', 'profile_1_1768481463.jpg'),
+(2, 'april', '37d153a06c79e99e4de5889dbe2e7c57', 'profile_2_1768481424.png');
 
 --
 -- Indexes for dumped tables
@@ -75,6 +102,12 @@ INSERT INTO `user` (`id`, `username`, `password`, `foto`) VALUES
 -- Indeks untuk tabel `article`
 --
 ALTER TABLE `article`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `gallery`
+--
+ALTER TABLE `gallery`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -94,10 +127,16 @@ ALTER TABLE `article`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
+-- AUTO_INCREMENT untuk tabel `gallery`
+--
+ALTER TABLE `gallery`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
